03817a349ff2626b84cefe3ae206f6fc55bd2589
Francesco <francesco.barabino@tiscali.it>
2020-04-27 19:47:39 +0200

    Bug fixing

--- Source/Transcoder.c
+++ Source/Transcoder.c
@@ -10,79 +10,84 @@ int main (int argc, char * argv[])
 	char * outLg = NULL;
 	int i;
 
-	if (strCompare(argv[1], "--out", 0) || strCompare(argv[1], "--in", 0))
+	if (argc> 1)	
 	{
-		if (argc < 7)
-			printf("SOME OPTIONS MISSING, TYPE 'help' FOR HELP\n");
-		for (i = 1; i < argc; i = i+3)
+		if (strCompare(argv[1], "--out", 0) || strCompare(argv[1], "--in", 0))
 		{
-			if (strCompare(argv[i], "--out", 0))
+			if (argc < 7)
+				printf("SOME OPTIONS MISSING, TYPE 'help' FOR HELP\n");
+			for (i = 1; i < argc; i = i+3)
 			{
-				if (strCompare(argv[i+1], "py", 0))
+				if (strCompare(argv[i], "--out", 0))
 				{
-					outLg = strCopy(argv[i+1]);
-					outName = strCopy(argv[i+2]);
+					if (strCompare(argv[i+1], "py", 0))
+					{
+						outLg = strCopy(argv[i+1]);
+						outName = strCopy(argv[i+2]);
 
+					}
+					else if (strCompare(argv[i+1], "c", 0))
+					{
+						outLg = strCopy(argv[i+1]);
+						outName = strCopy(argv[i+2]);
+					}
+					else
+						printf("YOU MUST SELECT A VALID LANGUAGE\n");
 				}
-				else if (strCompare(argv[i+1], "c", 0))
+				else if (strCompare(argv[i], "--in", 0))
 				{
-					outLg = strCopy(argv[i+1]);
-					outName = strCopy(argv[i+2]);
+					if (strCompare(argv[i+1], "py", 0))
+					{
+						inLg = strCopy(argv[i+1]);
+						inName = strCopy(argv[i+2]);
+					}
+					else if (strCompare(argv[i+1], "c", 0))
+					{
+						inLg = strCopy(argv[i+1]);
+						inName = strCopy(argv[i+2]);
+					}
+					else
+						printf("YOU MUST SELECT A VALID LANGAUGE\n");
 				}
 				else
-					printf("YOU MUST SELECT A VALID LANGUAGE\n");
+					printf("UNKNOWN COMMAND\n");
 			}
-			else if (strCompare(argv[i], "--in", 0))
-			{
-				if (strCompare(argv[i+1], "py", 0))
-				{
-					inLg = strCopy(argv[i+1]);
-					inName = strCopy(argv[i+2]);
-				}
-				else if (strCompare(argv[i+1], "c", 0))
-				{
-					inLg = strCopy(argv[i+1]);
-					inName = strCopy(argv[i+2]);
-				}
-				else
-					printf("YOU MUST SELECT A VALID LANGAUGE\n");
-			}
+
+			if (!inName)
+				printf("YOU MUST INSERT AN INPUT FILE NAME\n");
+			else if (!outName)
+				printf("YOU MUST INSERT AN OUTPUT FILE NAME\n");
+			else if (!inLg)
+				printf("YOU MUST INSERT AN INPUT FILE LANGUAGE\n");
+			else if (!outLg)
+				printf("YOU MUST INSERT AN OUTPUT FILE LANGUAGE\n");
 			else
-				printf("UNKNOWN COMMAND\n");
-		}
+			{
+				if (strCompare(inLg, "c", 0))
+					scan_C(inName);
+				else if (strCompare(inLg, "py", 0))
+					printf("PYTHON SCANNING NOT AVAILABLE (CURRENTLY UNDER DEVELOPMENT)\n");
+				if (strCompare(outLg, "py", 0))
+					print_Python(outName);
+				else if (strCompare(outLg, "c", 0))
+					printf("C PRINTING NOT AVAILABLE (CURRENTLY UNDER DEVELOPMENT)\n");
+			}
 
-		if (!inName)
-			printf("YOU MUST INSERT AN INPUT FILE NAME\n");
-		else if (!outName)
-			printf("YOU MUST INSERT AN OUTPUT FILE NAME\n");
-		else if (!inLg)
-			printf("YOU MUST INSERT AN INPUT FILE LANGUAGE\n");
-		else if (!outLg)
-			printf("YOU MUST INSERT AN OUTPUT FILE LANGUAGE\n");
-		else
+		}		
+	
+		else if (strCompare(argv[1], "help", 0))
 		{
-			if (strCompare(inLg, "c", 0))
-				scan_C(inName);
-			else if (strCompare(inLg, "py", 0))
-				printf("PYTHON SCANNING NOT AVAILABLE (CURRENTLY UNDER DEVELOPMENT)\n");
-			if (strCompare(outLg, "py", 0))
-				print_Python(outName);
-			else if (strCompare(outLg, "c", 0))
-				printf("C PRINTING NOT AVAILABLE (CURRENTLY UNDER DEVELOPMENT)\n");
+			printf("COMMANDS:\n");
+			printf("transcode\n");
+			printf("help\n");
+			printf("OPTIONS:\n");
+			printf("--out | Selects the output file\n");
+			printf("Accepts two arguments, the first one must be a language choice and the second one a file name\n");
+			printf("--in | Selects the input file\n");
+			printf("Accepts two arguments, the first must be a language choice and the second a file name\n");
 		}
-
-		
-	}
-	else if (strCompare(argv[1], "help", 0))
-	{
-		printf("COMMANDS:\n");
-		printf("transcode\n");
-		printf("help\n");
-		printf("OPTIONS:\n");
-		printf("--out | Selects the output file\n");
-		printf("Accepts two arguments, the first one must be a language choice and the second one a file name\n");
-		printf("--in | Selects the input file\n");
-		printf("Accepts two arguments, the first must be a language choice and the second a file name\n");
+		else
+			printf("UNKNOWN COMMAND\n");
 	}
 	else
 		printf("UNKNOWN COMMAND\n");
